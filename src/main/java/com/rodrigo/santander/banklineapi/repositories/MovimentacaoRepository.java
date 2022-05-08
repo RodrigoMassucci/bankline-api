6a0585c752560f54c2336ae24fe98c708f65acdc
RodrigoMassucci <rodrigo.massucci@outlook.com>
2022-05-08 19:46:03 -0300

    add novo endpoint

--- src/main/java/com/rodrigo/santander/banklineapi/repositories/MovimentacaoRepository.java
+++ src/main/java/com/rodrigo/santander/banklineapi/repositories/MovimentacaoRepository.java
@@ -1,9 +1,12 @@
 package com.rodrigo.santander.banklineapi.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rodrigo.santander.banklineapi.model.Movimentacao;
 
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer>  {
-
+	
+	public List<Movimentacao>findByIdConta(Integer idConta);
 }
